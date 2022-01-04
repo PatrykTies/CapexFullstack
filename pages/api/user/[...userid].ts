29e2656aa7c56bc5e55af7b5281872a9b2c66211
Patryk <patryk.ties@lovehomeswap.com>
2022-01-04 16:53:05 +0000

    fix dynamic route

--- pages/api/user/[...userid].ts
+++ pages/api/user/[...userid].ts
@@ -3,9 +3,11 @@ import prisma from '../../../lib/prisma';
 
 export default validateRoute(async (req, res, user) => {
   const { userid } = req.query;
+  // eslint-disable-next-line radix
+  const userId = parseInt(userid[0]);
   const allEntries = await prisma.entries.findMany({
     where: {
-      userId: userid,
+      userId,
     },
     include: {
       tasks: true,
